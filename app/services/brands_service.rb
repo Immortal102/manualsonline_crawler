9209edaa311c85691abb165f6818cfbaef370a08
Dmitry Stepanenko <sdm102@yandex.ua>
2018-02-20 17:27:02 +0200

    Update brands service to make it able to populate all the brands remotely.

--- app/services/brands_service.rb
+++ app/services/brands_service.rb
@@ -1,10 +1,13 @@
 class BrandsService < StartDataService
   CONFIG_PATH = "#{Rails.root.to_s}/config/brands.yml"
 
+  def initialize(remote=false)
+    @remote = remote ? 'remote' : 'local'
+    super()
+  end
+
   def prepared_to_insert
-    @prepared_to_insert ||= begin
-      matched.map { |mb_name| { name: mb_name, manualsonline_url: url_for(mb_name) } }
-    end
+    @prepared_to_insert ||= generate_method(__method__)
   end
 
   def populate_db!
@@ -15,8 +18,43 @@ class BrandsService < StartDataService
     end
   end
 
+  def errors
+    @errors.concat(remote_data_getter.errors)
+  end
+
   private
-  def url_for(brand_name)
-    base_url + '/' + brand_name.strip.gsub(/\s/, '_').gsub(/-/, '').downcase
+  def generate_method(prefix)
+    send("#{prefix}_#{@remote}")
+  end
+
+  def prepared_to_insert_local
+    # get matched brands with the urls from brands.yml
+    matched.map { |mb_name| { name: mb_name, manualsonline_url: url_for(allowed.find{|a| a[:name] == mb_name}[:slug]) }}
+  end
+
+  def prepared_to_insert_remote
+    # get all the brands with the urls from http://manualsonline.com/brands/
+    remote_data_getter.get_brands_data.map do |raw_data|
+      prepared_from_raw(raw_data)
+    end
+  end
+
+  def prepared_from_raw(data)
+    base_uri = URI(base_url)
+    b_name = data[:link_text]
+    uri = URI(data[:href])
+
+    uri.scheme = base_uri.scheme
+    uri.host = base_uri.host
+
+    { name: b_name, manualsonline_url: uri.to_s }
+  end
+
+  def remote_data_getter
+    @remote_data_getter ||= ManualsonlineDataService.new(base_url)
+  end
+
+  def url_for(slug)
+    base_url + '/' + slug
   end
 end
