5be2caa0cf45fd0d4a0a1007736b1f07cf4a4e4c
Dmitry Stepanenko <sdm102@yandex.ua>
2018-02-22 12:43:54 +0200

    Scopes.

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -4,4 +4,8 @@ class Brand < ApplicationRecord
   has_many :products, through: :brands_categories, dependent: :destroy
 
   scope :ordered, -> { order('created_at ASC') }
+
+  def empty_category_ids
+    category_ids - products.select('DISTINCT brands_categories.category_id as non_empty').map(&:non_empty)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   belongs_to :brands_category
   has_one :category, through: :brands_category
   has_one :brand, through: :brands_category
+
+  scope :for_categories, ->(c_ids) { joins(:brands_category).where("brands_categories.category_id IN (?)", Array.wrap(c_ids)) }
+  scope :for_brands, ->(b_ids) { joins(:brands_category).where("brands_categories.brand_id IN (?)", Array.wrap(b_ids)) }
 end
